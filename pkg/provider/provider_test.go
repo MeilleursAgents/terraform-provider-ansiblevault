d97f8f564b208ce4b64413db086b4a5c4e6388f5
Vincent Boutour <bob@vibioh.fr>
2019-06-06 19:53:10 +0200

    Checking internal validate of provider

--- pkg/provider/provider_test.go
+++ pkg/provider/provider_test.go
@@ -1,5 +1,27 @@
 package provider
 
+import "testing"
+
 const (
 	filesFolder = "../../files/"
 )
+
+func TestProvider(t *testing.T) {
+	var cases = []struct {
+		intention string
+		want      error
+	}{
+		{
+			"configuration is valid",
+			nil,
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.intention, func(t *testing.T) {
+			if result := Provider().InternalValidate(); result != testCase.want {
+				t.Errorf("Provider() = %#v, want %#v", result, testCase.want)
+			}
+		})
+	}
+}
